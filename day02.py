eb94b2d39d171bd76e04a5d6b2b8eff789194542
eun <bootcamp0eun@gmail.com>
2023-01-13 14:43:06 +0900

    day02 number2

--- day02.py
+++ day02.py
@@ -1,14 +1,15 @@
 # Chapter 03 숫자
+# 진법
 
-print(2*2*2*2*2)
-print(2**5)
-print(pow(2, 5))
+# number = 0b10011010   # 2진수
+# number = 0x9a 1    # 16진수
+# number = 0o232      # 8진수
+# print(number)
 
-print(divmod(100, 4))
-print(type(divmod(100, 4)))
-print(type((1, 2)))
-test = 1, 2    # packing
-print(type(test))
-a, b = test   # unpacking
-print(a)
-print(b)
+number = 154
+print(bin(number))
+print(oct(number))
+print(hex(number))
+
+print(ord("l"))     # 스페이스바에 대한 아스키코드 값 ascii
+print(hex(ord("l")))
