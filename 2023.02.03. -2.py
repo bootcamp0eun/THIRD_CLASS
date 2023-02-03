48e0c21d1f92a1cc9fdf45c34cf7f56bd7e9f012
eun <bootcamp0eun@gmail.com>
2023-02-03 14:13:27 +0900

    day15 linear list06 (asdljk03)

--- 2023.02.03. -2.py
+++ 2023.02.03. -2.py
@@ -1,4 +1,9 @@
 def print_Poly(p_x):
+    '''
+    다항식을 포맷에 맞게 출력하는 함수
+    :param p_x: 계수를 원소로 가지고 있는 리스트
+    :return: 다항식 문자열
+    '''
     term = len(p_x) - 1  # 최고차항 숫자 = 배열길이-1
     poly_str = "P(x) = "
 
@@ -17,6 +22,12 @@ def print_Poly(p_x):
 
 
 def calc_poly(x_val, p_x):
+    '''
+    다항식의 산술연산을 하는 함수
+    :param x_val: x값 integer
+    :param p_x: 계수를 원소로 가지고 있는 리스트
+    :return: 다항식 계산 결과 값 integer
+    '''
     ret_value = 0
     term = len(p_x) - 1  # 최고차항 숫자 = 배열길이-1
 
@@ -33,8 +44,7 @@ px = [3, -4, 0, 6]  # = 7x^3 -4x^2 +0x^1 +5x^0
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__":
-    pStr = print_Poly(px)
-    print(pStr)
+    print(print_Poly(px))
 
     x_value = int(input("X 값 : "))
 
