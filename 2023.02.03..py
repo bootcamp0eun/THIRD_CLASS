561e09440da4a24c1e27d53746d8dd01606fca71
eun <bootcamp0eun@gmail.com>
2023-02-03 11:16:28 +0900

    day15 linear list02

--- 2023.02.03..py
+++ 2023.02.03..py
@@ -1,3 +1,5 @@
+
+'''
 pokemons = list()
 
 
@@ -14,3 +16,30 @@ add_data('꼬부기')
 add_data('이상해')
 
 print(pokemons)
+'''
+
+
+
+def insert_data(idx, pokemon):
+    if idx < 0 or idx > len(pokemons):
+        print("데이터를 삽입할 범위를 벗어났습니다.")
+        return
+
+    pokemons.append(None)  # 빈칸 추가
+
+    for i in range(len(pokemons) - 1, idx, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
+
+    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
+
+
+if __name__ == "__main__":
+    pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
+    print(pokemons)
+    insert_data(2, '어니부기')
+    print(pokemons)
+    insert_data(6, '거북왕')
+    print(pokemons)
+
+
