cfe946c689419ce1cbed635947e0d253401dc554
eun <bootcamp0eun@gmail.com>
2023-02-03 11:30:24 +0900

    day15 linear list03

--- 2023.02.03..py
+++ 2023.02.03..py
@@ -19,7 +19,6 @@ print(pokemons)
 '''
 
 
-
 def insert_data(idx, pokemon):
     if idx < 0 or idx > len(pokemons):
         print("데이터를 삽입할 범위를 벗어났습니다.")
@@ -34,12 +33,35 @@ def insert_data(idx, pokemon):
     pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
 
 
+def delete_data(idx):
+    if idx < 0 or idx > len(pokemons):
+        print("데이터를 삭제할 범위를 벗어났습니다.")
+        return
+
+    len_pokemons = len(pokemons)
+    pokemons[idx] = None  # 데이터 삭제
+
+    for i in range(idx + 1, len_pokemons):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
+
+    del (pokemons[len_pokemons - 1])
+
+
 if __name__ == "__main__":
     pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
     print(pokemons)
-    insert_data(2, '어니부기')
+    delete_data(1)
+    print(pokemons)
+    delete_data(3)
+    print(pokemons)
+    '''
+    pokemons.insert(2, '어니부기')
+    #insert_data(2, '어니부기')
     print(pokemons)
     insert_data(6, '거북왕')
     print(pokemons)
+    '''
+
 
 
