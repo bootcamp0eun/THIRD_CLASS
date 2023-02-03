072b465ca06a35dc43ef52881f0961ca9bb18ff9
eun <bootcamp0eun@gmail.com>
2023-02-03 16:56:46 +0900

    day15 --WORK-- practice problem

---
+++ 2023.02.03.work2.py
@@ -0,0 +1,27 @@
+"""
+2023.02.03.금
+Chapter03 선형리스트
+연습문제
+
+1. 선형 리스트
+
+2. 1번
+
+3. 4 - 2 - 3 - 1
+
+4.
+katok.append(None)
+
+5.
+del(katok[-1])
+
+6.
+def add_data(friend) :
+	kLen = len(katok)
+	katok.append(None)
+	katok[kLen-1] = friend
+
+7. 1번
+
+8. 4번
+"""
