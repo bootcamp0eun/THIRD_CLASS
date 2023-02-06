b15fb78cb8e79bef85ba931d7c1ce02c3cba2bec
eun <bootcamp0eun@gmail.com>
2023-02-06 18:41:16 +0900

    day16 --WORK-- chapter05 practice problem

---
+++ 2023.02.06.work4.py
@@ -0,0 +1,28 @@
+"""
+2023.02.06.
+chapter 05 연습문제
+
+1.
+3번, 4번
+
+2.
+node1.link = node1
+
+3.
+1) node2
+2) node3
+3) node3
+
+4.
+2번
+
+5.
+1) current = head
+2) last = last.link
+3) last.link = current.link
+
+6.
+1) return current
+2) current.link != head
+3) current = current.link
+"""
