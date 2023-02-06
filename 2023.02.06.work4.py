03b5eae5b90a98e446c046650aac087cef5ccf0b
eun <bootcamp0eun@gmail.com>
2023-02-06 23:44:49 +0900

    day16 --WORK-- chapter06 ex 01

--- 2023.02.06.work4.py
+++ 2023.02.06.work4.py
@@ -1,28 +1,60 @@
-"""
-2023.02.06.
-chapter 05 연습문제
+def is_stack_empty():
+    global SIZE, stack, top
+    if top == -1:
+        return True
+    return False
 
-1.
-3번, 4번
 
-2.
-node1.link = node1
+def is_stack_full():
+    global SIZE, stack, top
+    if top == SIZE-1:
+        return True
+    return False
 
-3.
-1) node2
-2) node3
-3) node3
 
-4.
-2번
+def push(new):
+    global SIZE, stack, top
+    if is_stack_full():
+        return
+    top = top + 1
+    stack[top] = new
 
-5.
-1) current = head
-2) last = last.link
-3) last.link = current.link
 
-6.
-1) return current
-2) current.link != head
-3) current = current.link
-"""
+def peek():
+    global stack, top
+    if is_stack_empty():
+        return None
+    return stack[top]
+
+
+def pop():
+    global SIZE, stack, top
+    if is_stack_empty():
+        return None
+    temp = stack[top]
+    top = top - 1
+    return temp
+
+
+SIZE = 10
+stack = [None for i in range(SIZE)]
+top = -1
+
+if __name__=="__main__":
+    stone = ["주황", "초록", "파랑", "자주", "빨강", "노랑"]
+    for i in stone:
+        push(i)
+    print("과자집에 가는 길 : ", end='')
+    while not is_stack_empty():
+        print(pop(), end=" --> ")
+    print("과자집")
+
+    i = -1
+    while i >= -len(stone):
+        push(stone[i])
+        i = i - 1
+    print("우리집에 오는 길 : ", end='')
+    while not is_stack_empty():
+        print(pop(), end=" --> ")
+    print("우리집")
+
