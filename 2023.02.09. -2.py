f14c146d16e8cdb79b6fcb81d95f1b3c50e72da5
eun <bootcamp0eun@gmail.com>
2023-02-09 10:13:19 +0900

    day19 recursion 01 fibo

---
+++ 2023.02.09. -2.py
@@ -0,0 +1,22 @@
+def fibo_recu(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo_recu(n-1) + fibo_recu(n-2)
+
+
+def fibo_iter(n):
+    r = list()
+    p1, p2 = 1, 1
+    for _ in range(n):
+        r.append(p1)
+        p1, p2 = p2, p1+p2
+    return r[-1]
+
+
+print('피보나치 수 --> 0 1 ')
+for i in range(2, 40):
+    print(f'{i} : {fibo_iter(i)}')  # repetition
+    #print(f'{i} : {fibo_recu(i)}')   # recursion
